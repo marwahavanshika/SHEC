b9510bb4ee4535a24cc2999dd7b1b44e6a088669
Keshav <keshavbhutaani1360@gmail.com>
2023-12-27 17:50:26 +0530

    Finshed animation on Home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { ParallaxLayer, Parallax } from "@react-spring/parallax"
-import { useScroll, animated, config } from "@react-spring/web";
 import { useRef, useEffect, useState } from "react"
+import Animation from '../components/Animation'
 
 export default function Home() {
   const parallaxRef = useRef();
@@ -22,7 +22,10 @@ export default function Home() {
 
   return (
     <div>
-      <Parallax pages={8} ref={parallaxRef} className={' h-screen snap-y snap-mandatory'} config={config.stiff}>
+      <Parallax pages={8} ref={parallaxRef} className={' h-screen snap-y snap-mandatory'}>
+        <ParallaxLayer className={' text-white flex items-center justify-start px-32'} style={{backgroundImage:"url('/head.jpg')",backgroundSize:'cover',backgroundPosition:'cover'}}>
+          <p className={'font-[10] text-7xl'}>You Ask It<br />We Engineer it</p>
+        </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.25} className={' h-fit flex flex-col lg:flex-row items-center justify-center gap-5 md:gap-10 px-10'}>
           <div className={'flex flex-col flex-1 p-10'}>
             <h1 className={'text-3xl md:text-5xl lg:text-7xl mb-4 font-bold'}>Welcome, SHEC</h1>
@@ -37,12 +40,13 @@ export default function Home() {
             ))}
           </div>
         </ParallaxLayer>
-        <ParallaxLayer sticky={{ start: 2, end: 4 }} style={{ color: parallaxRef.current?.offset == 2 ? 'white' : 'black' }} className={'h-screen w-[50vw] bg-transparent flex flex-col p-32 justify-start items-start'}>
+        <ParallaxLayer sticky={{ start: 2, end: 4 }} style={{ color: parallaxRef.current?.offset == 2 ? 'white' : 'black' }} className={'h-screen w-[50vw] bg-transparent flex flex-col px-28 pb-20 justify-center items-start'}>
           <h1 className={'text-5xl'}>We Design for</h1>
           <h1 className={'text-7xl'} style={{ color: (page == 2) ? 'white' : 'black' }} >Layouts</h1>
           <h1 className={'text-7xl'} style={{ color: (page == 3) ? 'white' : 'black' }} >Buildings</h1>
           <h1 className={'text-7xl'} style={{ color: (page == 4) ? 'white' : 'black' }} >Stadiums</h1>
         </ParallaxLayer>
+        <ParallaxLayer sticky={{start:2, end:4}} className={'bg-transparent flex items-center justify-end px-20 pt-10'} ><Animation stateNumber={page-1} /></ParallaxLayer>
         <ParallaxLayer offset={2} className={'h-screen bg-blue-500'}></ParallaxLayer>
         <ParallaxLayer offset={3} className={'h-screen bg-blue-500'}></ParallaxLayer>
         <ParallaxLayer offset={4} className={'h-screen bg-blue-500'}></ParallaxLayer>
